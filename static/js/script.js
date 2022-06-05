d80ad6cbcb44e381d9c3950044f9fb64ab2c3503
smhenderson89 <scott.mc.henderson@gmail.com>
2022-06-04 21:47:35 -0700

    updating CSS styling

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+monthObject = {
+    "January (1)" : 31,
+    "February (2)" : 29,
+    "March (3)" : 31,
+    "April (4)" : 30,
+    "May (5)" : 31,
+    "June (6)" : 30,
+    "July (7)" : 31,
+    "August (8)" : 31,
+    "September (9)" : 30,
+    "October (10)" : 31,
+    "November (11)" : 30,
+    "December (12)" : 31
+};
+
+dayArray = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+window.onload = function () {
+    var monthSelect = document.getElementById("monthrange");
+    var daySelect = document.getElementById("dayrange");
+
+}
